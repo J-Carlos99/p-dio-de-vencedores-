09a1ae375a8f393a75e4916c9970348c21d356bc
J-Carlos99 <87335255+J-Carlos99@users.noreply.github.com>
2023-03-03 00:21:11 -0300

    Create CursoJava.java

---
+++ CursoJava.java
@@ -0,0 +1,22 @@
+class CursoJava {
+  public static void main(String[] args) {
+    int pos = 1;
+    switch(pos){
+      case 1:
+        System.out.println("Primeiro Lugar");
+        break;
+      case 2:
+        System.out.println("Segundo Lugar");
+        break;
+      case 3:
+        System.out.println("Terceiro Lugar");
+        break;
+      case 4: case 5: case 6:
+        System.out.println("Premio de participação");
+        break;
+      default:
+        System.out.println("Não ganhou premio");
+        break;
+    }
+  }
+}
